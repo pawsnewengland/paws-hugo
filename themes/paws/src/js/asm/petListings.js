b54aedb642d472d966a233a24ad3d167b9f9c9de
cferdinandi <chris@gomakethings.com>
2018-11-23 08:00:14 -0500

    Fixed bug

--- themes/paws/src/js/asm/petListings.js
+++ themes/paws/src/js/asm/petListings.js
@@ -51,13 +51,13 @@ var petListings = function () {
 		// Get URLs with protocol
 		txt = txt.replace(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/gi, function (match) {
 			links.push('<a href="' + match + '">' + match + '</a>');
-			return '<link-' + (links.length - 1) + '>';
+			return '{{link-' + (links.length - 1) + '}}';
 		});
 
 		// Get email addresses
 		txt = txt.replace(/^([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22))*\x40([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d))*(\.\w{2,})+$/gi, function (match) {
 			links.push('<a href="mailto:' + match + '">' + match + '</a>');
-			return '<link-' + (links.length - 1) + '>';
+			return '{{link-' + (links.length - 1) + '}}';
 		});
 
 		// Put all links into the string
